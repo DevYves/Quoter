3b035a502839eaf68c64897f8dd9d53ed24fb2b8
Yves Freypons <yfreypons@gmail.com>
2019-08-23 10:15:03 -0700

    Setup html skeleton and basic styling
    Reorder css alphebetically

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,103 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js App" />
+    <h1 class="title">Quoter</h1>
+    <div class="progress-bar-outer">
+      <div class="progress-bar-inner">1/10</div>
+    </div>
+    <form class="quote-form" action method="post" titl="Quote">
+      <label for="quote">Quote</label>
+      <input class="quote-text" type="text" name="quote" id />
+      <div class="button-container">
+        <input type="submit" value="Random" />
+        <input type="submit" value="Add Quote" />
+      </div>
+    </form>
+
+    <div class="quote-container">
+      <div class="quote">Test quote</div>
+      <div class="quote">Test quote</div>
+      <div class="quote">Test quote</div>
+      <div class="quote">Test quote</div>
+      <div class="quote">Test quote</div>
+      <div class="quote">Test quote</div>
+      <div class="quote">Test quote</div>
+      <div class="quote">Test quote</div>
+      <div class="quote">Test quote</div>
+      <div class="quote">Test quote</div>
+    </div>
+    <div class="footer-info">Info: click on a quote to delete it</div>
   </div>
 </template>
 
 <script>
-import HelloWorld from "./components/HelloWorld.vue";
-
-export default {
-  name: "app",
-  components: {
-    HelloWorld
-  }
-};
+export default {};
 </script>
 
 <style>
 #app {
-  font-family: "Avenir", Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
+  align-content: center;
+  align-items: center;
+  display: flex;
+  flex-flow: column nowrap;
+  height: 100%;
+  justify-content: start;
+}
+.progress-bar-outer {
+  background-color: #ececec;
+  border-radius: 5px;
+  margin: 10px;
+  width: 50%;
+}
+.progress-bar-inner {
+  background-color: turquoise;
+  border-radius: 5px 0px 0px 5px;
+  padding: 2px;
   text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+  transition: width 0.3s ease;
+  width: 10%;
+}
+.button-container {
+  display: flex;
+  justify-content: space-evenly;
+  width: 100%;
+}
+.quote-form {
+  align-items: center;
+  display: flex;
+  flex-flow: column nowrap;
+  height: 20%;
+  justify-content: center;
+  width: 50%;
+}
+.quote-text {
+  border-radius: 5px;
+  height: 90%;
+  margin: 10px;
+  width: 100%;
+}
+.quote-container {
+  align-items: flex-start;
+  display: flex;
+  flex-wrap: wrap;
+  height: 60%;
+  justify-content: center;
+  margin-top: 10px;
+  width: 80%;
+}
+.quote {
+  border: 1px solid black;
+  margin: 10px;
+  text-align: center;
+}
+.footer-info {
+  background-color: turquoise;
+  border-radius: 5px;
+  margin-bottom: 25px;
+  padding: 10px;
+  text-align: center;
+  width: 50%;
+}
+.title {
+  height: 10%;
 }
 </style>
